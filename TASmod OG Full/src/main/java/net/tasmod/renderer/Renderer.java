81fc40d29e924c54a92d629b00be681c4b992e5f
Pfannekuchen <gamesupport@mgnet.work>
2022-08-02 17:18:28 +0200

    [WIP] Adding audio to render

--- TASmod OG Full/src/main/java/net/tasmod/renderer/Renderer.java
+++ TASmod OG Full/src/main/java/net/tasmod/renderer/Renderer.java
@@ -1,7 +1,6 @@
 package net.tasmod.renderer;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.ProcessBuilder.Redirect;
@@ -24,18 +23,21 @@ public final class Renderer extends Replayer {
 	public static ALExt ext;
 	
 	private OutputStream stream;
+	private OutputStream stream2;
 	private int framesPerTick;
 	private ByteBuffer b;
 	private byte[] ba;
 	private ByteBuffer aa = ByteBuffer.allocate(2 * 2205 * 4);
 	private Timer timer;
+	private Process videoProcess;
+	private Process audioProcess;
 	
 	public String path;
 	public String resolution = "1920x1080";
 	public int framerate = 60;
 	public int crf = 18;
 	public String codec = "libx264";
-	public FileOutputStream out;
+	public boolean startRender;
 	
 	public Renderer(File name) throws Exception {
 		super(name);
@@ -48,7 +50,8 @@ public final class Renderer extends Replayer {
 	public void startReplay() {
 		super.startReplay();
 		
-		String ffmpeg = '"' + path + '"' + " -y -f rawvideo -c:v rawvideo -s 1920x1080 -pix_fmt rgb24 -r " + framerate + " -i - -vf vflip -pix_fmt yuv420p -c:v " + codec + " -s " + resolution + ("libx264".equals(codec) ? " -preset veryslow -tune film -profile high -rc-lookahead 120" : " -tier high -preset 4 -la_depth 120 -sc_detection true -hielevel 4level") + " -qp " + crf + " -crf " + crf + " \"" + file.getName() + ".mp4.video\"";
+		// start video ffmpeg
+		String ffmpeg = '"' + path + '"' + " -y -f rawvideo -c:v rawvideo -s 1920x1080 -pix_fmt rgb24 -r " + framerate + " -i - -vf vflip -pix_fmt yuv420p -c:v " + codec + " -s " + resolution + ("libx264".equals(codec) ? " -preset veryslow -tune film -profile high -rc-lookahead 120" : " -tier high -preset 4 -la_depth 120 -sc_detection true -hielevel 4level") + " -qp " + crf + " -an -sn -crf " + crf + " \"" + file.getName() + ".mp4\"";
 		System.out.println(ffmpeg);
 		ProcessBuilder pb = new ProcessBuilder(ffmpeg);
 		pb.redirectOutput(Redirect.INHERIT);
@@ -56,21 +59,30 @@ public final class Renderer extends Replayer {
 		pb.redirectError(Redirect.INHERIT);
 		this.framesPerTick = 0;
 		try {
-			Process p = pb.start();
-			this.stream = p.getOutputStream();
+			videoProcess = pb.start();
+			this.stream = videoProcess.getOutputStream();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// start audio ffmpeg
+		ffmpeg = '"' + path + '"' + " -y -f f32le -ar 44100 -ac 2 -i - -c:a libopus -b:a 320K -vn -sn -vbr on \"" + file.getName() + ".opus\"";
+		System.out.println(ffmpeg);
+		pb = new ProcessBuilder(ffmpeg);
+		pb.redirectOutput(Redirect.INHERIT);
+		pb.redirectErrorStream(true);
+		pb.redirectError(Redirect.INHERIT);
+		try {
+			audioProcess = pb.start();
+			this.stream2 = audioProcess.getOutputStream();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
 		try {
-			out = new FileOutputStream(new File(file.getName() + ".snd"));
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		
-		try {
 			this.timer = Utils.obtainTimerInstance();
 			this.timer.renderPartialTicks = 0f;
+			this.timer.elapsedTicks = 1;
 			TickrateChanger.updateTickrate(500.0f);
 			TickrateChanger.shouldOverrideRenderTime = true;
 		} catch (Exception e) {
@@ -87,6 +99,7 @@ public final class Renderer extends Replayer {
 		
 		try {
 			if (this.mc != null) {
+				this.startRender = true;
 				this.framesPerTick++;
 				if (this.framesPerTick == (framerate / 20)) {
 					this.timer.elapsedTicks = 1;
@@ -99,15 +112,14 @@ public final class Renderer extends Replayer {
 				if (b == null) {
 					this.b = ByteBuffer.allocateDirect(this.mc.displayWidth*this.mc.displayHeight*3);
 					this.ba = new byte[this.mc.displayWidth*this.mc.displayHeight*3];
-				} else {
-					this.b.clear();
-					GL11.glReadPixels(0, 0, this.mc.displayWidth, this.mc.displayHeight, GL11.GL_RGB, GL11.GL_UNSIGNED_BYTE, this.b);
-					this.b.get(this.ba);
-					try {
-						this.stream.write(this.ba);
-					} catch (Exception e1) {
-						e1.printStackTrace();
-					}
+				}
+				this.b.clear();
+				GL11.glReadPixels(0, 0, this.mc.displayWidth, this.mc.displayHeight, GL11.GL_RGB, GL11.GL_UNSIGNED_BYTE, this.b);
+				this.b.get(this.ba);
+				try {
+					this.stream.write(this.ba);
+				} catch (Exception e1) {
+					e1.printStackTrace();
 				}
 			}
 		} catch (Exception e) {
@@ -122,24 +134,40 @@ public final class Renderer extends Replayer {
 	public void tick() {
 		super.tick();
 		
-		if (out != null && dev != null) {
-			try {
-        		ext.alcRenderSamplesSOFT(dev, aa, 2205);
-				out.write(aa.array());
-				aa.clear();
-			} catch (IOException e) {
-				e.printStackTrace();
+		try {
+			if (this.stream2 != null && this.mc != null && this.startRender) {
+				ext.alcRenderSamplesSOFT(dev, this.aa, 2205);
+				this.stream2.write(this.aa.array());
+				this.aa.clear();
 			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		
 		if (TASmod.playback == null) { // playback ended
 			try {
-				stream.flush();
-				stream.close();
-				out.flush();
-				out.close();
-				out = null;
+				this.stream.flush();
+				this.stream.close();
+				this.stream2.flush();
+				this.stream2.close();
 				TickrateChanger.toggleTickadvance();
+				if (videoProcess.isAlive())
+					videoProcess.waitFor();
+				if (audioProcess.isAlive())
+					audioProcess.waitFor();
+				String ffmpeg = '"' + path + '"' + " -y -i \"" + file.getName() + ".mp4\" -i \"" + file.getName() + ".opus\" -c copy " + file.getName() + ".mkv\"";
+				ProcessBuilder pb = new ProcessBuilder(ffmpeg);
+				pb.redirectOutput(Redirect.INHERIT);
+				pb.redirectErrorStream(true);
+				pb.redirectError(Redirect.INHERIT);
+				try {
+					Process p = pb.start();
+					p.waitFor();
+					new File(file.getName() + ".mp4").delete();
+					new File(file.getName() + ".opus").delete();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
