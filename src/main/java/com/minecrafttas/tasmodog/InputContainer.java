cb159117ecd1e4f392b71cae81cbf37ec7c418c0
PancakeTAS <pancake@mgnet.work>
2023-05-05 12:51:33 +0200

    Create structs

---
+++ src/main/java/com/minecrafttas/tasmodog/InputContainer.java
@@ -0,0 +1,15 @@
+package com.minecrafttas.tasmodog;
+
+import java.util.List;
+
+import com.minecrafttas.tasmodog.structs.Tick;
+
+/**
+ * Input container containing inputs and other tasmod data for all ticks
+ * @author Pancake
+ */
+public class InputContainer {
+
+	private List<Tick> ticks;
+	
+}
