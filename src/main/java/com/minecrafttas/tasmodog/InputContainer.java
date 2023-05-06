2e351391606da306809ea180f872e05d89af8bf4
PancakeTAS <pancake@mgnet.work>
2023-05-07 00:19:25 +0200

    Implement new virtual input system

--- src/main/java/com/minecrafttas/tasmodog/InputContainer.java
+++ src/main/java/com/minecrafttas/tasmodog/InputContainer.java
@@ -1,5 +1,9 @@
 package com.minecrafttas.tasmodog;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.minecrafttas.tasmodog.structs.Tick;
@@ -11,5 +15,91 @@ import com.minecrafttas.tasmodog.structs.Tick;
 public class InputContainer {
 
 	private List<Tick> ticks;
+	private Tick currentTick;
+	private int nextTick;
+	private boolean isActive, isRecording;
 	
+	/**
+	 * Initialize Input container
+	 */
+	public InputContainer() {
+		this.ticks = new ArrayList<>();
+		this.currentTick = new Tick();
+		this.nextTick = 0;
+		this.isActive = true;
+		this.isRecording = false;
+	}
+	
+	/**
+	 * Post Initialize Input container with TASmod instance
+	 * @param tasmod TASmod instance
+	 */
+	public void init(TASmod tasmod) {
+		
+	}
+	
+	/**
+	 * Tick Input container
+	 */
+	public void tick() {
+		if (!this.isActive) {
+			this.currentTick = new Tick();
+			return;
+		}
+		
+		this.currentTick = (this.ticks.size() >= this.nextTick || this.isRecording) ? new Tick() : this.ticks.get(this.nextTick);
+		this.nextTick++;
+	}
+	
+	/**
+	 * Load tick data from file
+	 * @param file File
+	 * @throws Exception Filesystem exception
+	 */
+	@SuppressWarnings("unchecked")
+	public void load(File file) throws Exception {
+		ObjectInputStream oos = new ObjectInputStream(new FileInputStream(file));
+		this.ticks = (List<Tick>) oos.readObject();
+		oos.close();
+	}
+	
+	/**
+	 * Set input container into recording mode
+	 * @param isRecording Recording mode
+	 */
+	public void setRecording(boolean isRecording) {
+		this.isRecording = isRecording;
+	}
+	
+	/**
+	 * Disables the input container
+	 */
+	public void disable() {
+		this.isActive = false;
+	}
+
+	/**
+	 * Return current tick data
+	 * @return Current tick data
+	 */
+	public Tick getCurrentTickData() {
+		return this.currentTick;
+	}
+
+	/**
+	 * Return recording state
+	 * @return Recording state
+	 */
+	public boolean isRecording() {
+		return this.isRecording;
+	}
+
+	/**
+	 * Return active state
+	 * @return Active state
+	 */
+	public boolean isActive() {
+		return this.isActive;
+	}
+
 }
